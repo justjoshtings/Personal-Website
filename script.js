d474383b6c127902317baf8c22e05b55ff6709d3
justjoshtings <joshting1@gmail.com>
2018-03-21 00:22:31 -0400

    Update 3/20
    - Improved mobile friendly mode
    - Still lots of things to fix

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+
 function openNav() {
-	document.getElementById('side-menu').style.width = "16vw";
+	if (window.screen.width <=  667) {
+		document.getElementById('side-menu').style.width = '100vw';		
+	}
+	else {
+		document.getElementById('side-menu').style.width = '16vw';		
+	}
 }
 
 function closeNav() {
 	document.getElementById('side-menu').style.width = '0';
 }
-
