6f063c290ecb220d336c13b216e3ac1c35f222e6
justjoshtings <joshting1@gmail.com>
2018-03-28 13:51:43 -0400

    about.html updates

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function openNav() {
 function closeNav() {
 	document.getElementById('side-menu').style.width = '0';
 }
+
+var terms = $("ul li");
+
+	function rotateTerm() {
+
+	 var ct = $("#rotate").data("term") || 0;
+
+	 console.log(terms.eq([ct]).text());
+	 
+	  $("#rotate").data("term", 
+	  	ct == terms.length -1 ? 0 : ct + 1).text(terms.eq([ct]).text())
+	  .fadeIn().delay(3000).fadeOut(200, rotateTerm);
+
+	}
+	$(rotateTerm);
